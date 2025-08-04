6035acb3e807e9e3eeebb1cf2c836d496ab4420f
nirendra123 <nnirendrabajracharya13@gmail.com>
2025-08-04 20:00:25 +0545

    added welcome page

--- App.tsx
+++ App.tsx
@@ -6,12 +6,14 @@
  */
 
 import { StyleSheet, View } from 'react-native';
-import LoginScreen from './src/Screens/LoginScreen';
+// import LoginScreen from './src/Screens/LoginScreen';
+import WelcomeScreen from './src/Screens/WelcomeScreen';
 
 function App() {
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      {/* <LoginScreen /> */}
+      <WelcomeScreen />
     </View>
   );
 }
