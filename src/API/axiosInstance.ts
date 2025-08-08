f352df7dcc544af13a35e1c749c42566bee2affc
nirendra123 <nnirendrabajracharya13@gmail.com>
2025-08-08 13:02:58 +0545

    search movie using keyword and nevagite to movie detail

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_TOKEN =
+export const API_TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2I2NGVmMjFkYTAxODRlMzEwN2JkNGU1MzY2NTQ0OSIsIm5iZiI6MTc1MTAzOTI3NS41MjYsInN1YiI6IjY4NWViZDJiYTgzY2NkODFmNDkxNTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sMiHvjezkASUDT2xysvlVdD21tYRrjB2GoJDOVlRIMc';
 
 const api = axios.create({
--- src/Screens/Tabs/SearchTab.tsx
+++ src/Screens/Tabs/SearchTab.tsx
@@ -1,10 +1,110 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-export default function SearchTab() {
+import { TextInput } from 'react-native';
+import { Movie } from '../../Navigation/types';
+import api from '../../API/axiosInstance';
+
+export default function SearchTab({ navigation }: any) {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const searchMovies = async (searchQuery: string) => {
+    if (!searchQuery.trim()) {
+      setResults([]);
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await api.get('/search/movie', {
+        params: { query: searchQuery },
+      });
+      setResults(res.data.results || []);
+    } catch (error) {
+      console.error('Error searching movies:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setResults([]);
+      setLoading(false);
+      return;
+    }
+    const delay = setTimeout(() => {
+      searchMovies(query);
+    }, 500);
+    return () => clearTimeout(delay);
+  }, [query]);
   return (
-    <View>
-      <Text>SearchTab</Text>
+    <View style={styles.container}>
+      <View>
+        <TextInput
+          placeholder="search..."
+          value={query}
+          onChangeText={setQuery}
+          style={styles.input}
+        />
+      </View>
+
+      {loading && <Text>Loading...</Text>}
+      <FlatList
+        data={results}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.movieItem}
+            onPress={() =>
+              navigation.navigate('MovieDetails', { movieId: item.id })
+            }
+          >
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w200${item.poster_path}`,
+              }}
+              style={styles.poster}
+            />
+            <View style={{ flex: 1 }}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text numberOfLines={2} style={styles.overview}>
+                {item.overview || 'No description available'}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 10 },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  movieItem: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    backgroundColor: '#f8f8f8',
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  poster: { width: 80, height: 120, marginRight: 10 },
+  title: { fontSize: 16, fontWeight: 'bold' },
+  overview: { fontSize: 12, color: '#555' },
+});
